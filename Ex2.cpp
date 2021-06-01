6b2ab49339e322c79026e851f9bc29d069406c66
sl1kAd <80388890+sl1kAd@users.noreply.github.com>
2021-06-01 17:28:09 +0300

    Add files via upload

---
+++ Ex2.cpp
@@ -0,0 +1,241 @@
+﻿#include <iostream>
+
+typedef struct Node 
+{
+    int value;
+    struct Node* next;
+} Node;
+
+void push(Node** head, int data) 
+{
+    Node* tmp = (Node*)malloc(sizeof(Node));
+    tmp->value = data;
+    tmp->next = (*head);
+    (*head) = tmp;
+}
+
+int pop(Node** head) 
+{
+    Node* prev = NULL;
+    int val;
+    if (head == NULL) {
+        exit(-1);
+    }
+    prev = (*head);
+    val = prev->value;
+    (*head) = (*head)->next;
+    free(prev);
+    return val;
+}
+
+Node* getNth(Node* head, int n) 
+{
+    int counter = 0;
+    while (counter < n && head) 
+    {
+        head = head->next;
+        counter++;
+    }
+    return head;
+}
+
+
+Node* getLast(Node* head) 
+{
+    if (head == NULL) 
+    {
+        return NULL;
+    }
+    while (head->next) 
+    {
+        head = head->next;
+    }
+    return head;
+}
+
+void pushBack(Node* head, int value) {
+    Node* last = getLast(head);
+    Node* tmp = (Node*)malloc(sizeof(Node));
+    tmp->value = value;
+    tmp->next = NULL;
+    last->next = tmp;
+}
+
+Node * getLastButOne(Node * head) {
+    if (head == NULL) {
+        exit(-2);
+    }
+    if (head->next == NULL) {
+        return NULL;
+    }
+    while (head->next->next) {
+        head = head->next;
+    }
+    return head;
+}
+
+void popBack(Node * *head) {
+    Node* lastbn = NULL;
+    //Получили NULL
+    if (!head) {
+        exit(-1);
+    }
+    //Список пуст
+    if (!(*head)) {
+        exit(-1);
+    }
+    lastbn = getLastButOne(*head);
+    //Если в списке один элемент
+    if (lastbn == NULL) {
+        free(*head);
+        *head = NULL;
+    }
+    else {
+        free(lastbn->next);
+        lastbn->next = NULL;
+    }
+}
+
+int popBack(Node * *head) 
+{
+    Node* pFwd = NULL;  //текущий узел
+    Node* pBwd = NULL;  //предыдущий узел
+    //Получили NULL
+    if (!head) {
+        exit(-1);
+    }
+    //Список пуст
+    if (!(*head)) {
+        exit(-1);
+    }
+
+    pFwd = *head;
+    while (pFwd->next) {
+        pBwd = pFwd;
+        pFwd = pFwd->next;
+    }
+
+    if (pBwd == NULL) {
+        free(*head);
+        *head = NULL;
+    }
+    else {
+        free(pFwd->next);
+        pBwd->next = NULL;
+    }
+}
+
+void insert(Node * head, unsigned n, int val) {
+    unsigned i = 0;
+    Node* tmp = NULL;
+    //Находим нужный элемент. Если вышли за пределы списка, то выходим из цикла,
+    //ошибка выбрасываться не будет, произойдёт вставка в конец
+    while (i < n && head->next) {
+        head = head->next;
+        i++;
+    }
+    tmp = (Node*)malloc(sizeof(Node));
+    tmp->value = val;
+    //Если это не последний элемент, то next перекидываем на следующий узел
+    if (head->next) {
+        tmp->next = head->next;
+        //иначе на NULL
+    }
+    else {
+        tmp->next = NULL;
+    }
+    head->next = tmp;
+}
+
+int deleteNth(Node** head, int n) 
+{
+    if (n == 0) {
+        return pop(head);
+    }
+    else {
+        Node* prev = getNth(*head, n - 1);
+        Node* elm = prev->next;
+        int val = elm->value;
+
+        prev->next = elm->next;
+        free(elm);
+        return val;
+    }
+}
+
+
+void deleteList(Node** head) {
+    while ((*head)->next) {
+        pop(head);
+        *head = (*head)->next;
+    }
+    free(*head);
+}
+
+void deleteList(Node * *head) {
+    Node* prev = NULL;
+    while ((*head)->next) {
+        prev = (*head);
+        (*head) = (*head)->next;
+        free(prev);
+    }
+    free(*head);
+}
+
+void fromArray(Node * *head, int* arr, size_t size) {
+    size_t i = size - 1;
+    if (arr == NULL || size == 0) {
+        return;
+    }
+    do {
+        push(head, arr[i]);
+    } while (i-- != 0);
+}
+
+int* toArray(const Node * head) {
+    int leng = length(head);
+    int* values = (int*)malloc(leng * sizeof(int));
+    while (head) {
+        values[--leng] = head->value;
+        head = head->next;
+    }
+    return values;
+}
+
+void printLinkedList(const Node * head) {
+    while (head) {
+        printf("%d ", head->value);
+        head = head->next;
+    }
+    printf("\n");
+}
+
+void main() {
+    Node* head = NULL;
+    int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+    //Создаём список из массива
+    fromArray(&head, arr, 10);
+
+    printLinkedList(head);
+
+    //Вставляем узел со значением 333 после 4-го элемента (станет пятым)
+    insert(head, 4, 333);
+    printLinkedList(head);
+
+    pushBack(head, 11);
+    pushBack(head, 12);
+    pushBack(head, 13);
+    pushBack(head, 14);
+    printLinkedList(head);
+
+    printf("%d\n", pop(&head));
+    printf("%d\n", popBack(&head));
+
+    printLinkedList(head);
+    //Удаляем пятый элемент (индексация с нуля)
+    deleteNth(&head, 4);
+    printLinkedList(head);
+    deleteList(&head);
+
+    getch();
+}
---
+++ Ex3.cpp
@@ -0,0 +1,164 @@
+﻿#include <iostream>
+
+Node tmp;
+*c = NULL;
+if (a == NULL) 
+{
+    *c = b;
+    return;
+}
+if (b == NULL) 
+{
+    *c = a;
+    return;
+}
+
+if (a->value < b->value) 
+{
+    *c = a;
+    a = a->next;
+}
+else 
+{
+    *c = b;
+    b = b->next;
+}
+
+while (a && b) 
+{
+    if (a->value < b->value) 
+    {
+        (*c)->next = a;
+        a = a->next;
+    }
+    else 
+    {
+        (*c)->next = b;
+        b = b->next;
+    }
+    (*c) = (*c)->next;
+}
+
+if (a) 
+{
+    while (a) {
+        (*c)->next = a;
+        (*c) = (*c)->next;
+        a = a->next;
+    }
+}
+if (b) 
+{
+    while (b) 
+    {
+        (*c)->next = b;
+        (*c) = (*c)->next;
+        b = b->next;
+    }
+}
+
+
+void merge(Node * a, Node * b, Node * *c) 
+{
+    Node tmp;
+    *c = NULL;
+    if (a == NULL) 
+    {
+        *c = b;
+        return;
+    }
+    if (b == NULL) 
+    {
+        *c = a;
+        return;
+    }
+    if (a->value < b->value)
+    {
+        *c = a;
+        a = a->next;
+    }
+    else
+    {
+        *c = b;
+        b = b->next;
+    }
+    tmp.next = *c;
+    while (a && b) 
+    {
+        if (a->value < b->value) 
+        {
+            (*c)->next = a;
+            a = a->next;
+        }
+        else 
+        {
+            (*c)->next = b;
+            b = b->next;
+        }
+        (*c) = (*c)->next;
+    }
+    if (a) 
+    {
+        while (a) 
+        {
+            (*c)->next = a;
+            (*c) = (*c)->next;
+            a = a->next;
+        }
+    }
+    if (b) 
+    {
+        while (b) 
+        {
+            (*c)->next = b;
+            (*c) = (*c)->next;
+            b = b->next;
+        }
+    }
+    *c = tmp.next;
+}
+
+void split(Node * src, Node * *low, Node * *high) 
+{
+    Node* fast = NULL;
+    Node* slow = NULL;
+
+    if (src == NULL || src->next == NULL) 
+    {
+        (*low) = src;
+        (*high) = NULL;
+        return;
+    }
+
+    slow = src;
+    fast = src->next;
+
+    while (fast != NULL) 
+    {
+        fast = fast->next;
+        if (fast != NULL) 
+        {
+            fast = fast->next;
+            slow = slow->next;
+        }
+    }
+
+    (*low) = src;
+    (*high) = slow->next;
+    slow->next = NULL;
+}
+
+void mergeSort(Node * *head) 
+{
+    Node* low = NULL;
+    Node* high = NULL;
+    if ((*head == NULL) || ((*head)->next == NULL)) 
+    {
+        return;
+    }
+
+    split(*head, &low, &high);
+    mergeSort(&low);
+    mergeSort(&high);
+    merge(low, high, head);
+}
