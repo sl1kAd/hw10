0f84f9d713a85bd50ee0102f762597652351d2d1
sl1kAd <80388890+sl1kAd@users.noreply.github.com>
2021-06-01 17:05:30 +0300

    Add files via upload

---
+++ ConsoleApplication6.cpp
@@ -0,0 +1,43 @@
+﻿#include <stdio.h>
+#include <iostream>
+
+char* brackets_check(const char* s) 
+{
+    switch (*s) 
+    {
+    case '\0':
+    case ')':
+    case '}':
+    case ']':
+        return (char*)s;
+    case '(': 
+    {
+        char* ret = brackets_check(s + 1);
+        return (*ret == ')') ? brackets_check(ret + 1) : (char*)s;
+    }
+    case '{': 
+    {
+        char* ret = brackets_check(s + 1);
+        return (*ret == '}') ? brackets_check(ret + 1) : (char*)s;
+    }
+    case '[': 
+    {
+        char* ret = brackets_check(s + 1);
+        return (*ret == ']') ? brackets_check(ret + 1) : (char*)s;
+    }
+    default:
+        return brackets_check(s + 1);
+    }
+}
+
+int main(void) 
+{
+    setlocale(LC_ALL, "Russian");
+
+    char buf[BUFSIZ];
+
+    while (printf("Введите последовательность: ") && fgets(buf, BUFSIZ, stdin) && *buf != '\n')
+        printf("%s\n", (*brackets_check(buf)) ? "НЕПРАВИЛЬНАЯ" : "ПРАВИЛЬНАЯ");
+
+    return 0;
+}
